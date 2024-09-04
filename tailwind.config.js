755e0c0ab08f11d17abf41b9a57e340d8240d9a4
Rofi Dzaki Abdul Aziz <rofidzakismktibazma@gmail.com>
2024-09-04 15:12:22 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      color : {
+          primary : '#14b8a6'
+      }
+    },
+  },
+  plugins: [],
+}
+
